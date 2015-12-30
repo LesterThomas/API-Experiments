37381cf59cb8be29933674f25c4c52e66e30f021
Lester <lesterthomas@hotmail.com>
2015-12-30 17:46:10 +0000

    Minor fixes

--- serviceProvider/routes/api.js
+++ serviceProvider/routes/api.js
@@ -8,6 +8,10 @@ var integrationPoints = require('./integrationPoint');
 router.use('/catalogue', catalogue);
 router.use('/integrationPoint', integrationPoints);
 
-
+router.get('/', function(httprequest, httpresponse) {
+	httpresponse.contentType("application/json");
+	var objectResp={ catalogue:"http://lesterthomas.ddns.net:3002/api/catalogue", integrationPoints:"http://lesterthomas.ddns.net:3002/api/integrationPoint"};
+	httpresponse.send(JSON.stringify(objectResp));
+});
 
 module.exports = router;
